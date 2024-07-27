ca98689a011644f4189ea6ed396cd4baf3edaa01
Julio Martins <juliomarttins5@gmail.com>
2024-07-27 00:18:26 -0300

    feat: added new methods to linkedlist
    new methods added to linkedlist for reverse and for remove_value

--- computer-science/index.py
+++ computer-science/index.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class LinkedList:
     def __init__(self, value=None, nxt=None) -> None:
         self.value = value
@@ -91,33 +94,61 @@ class LinkedList:
         return last
 
     def insert(self, index, value):
-        crrIndex = -1
+        crr_index = -1
         prev, nxt = None, self
 
         for node in self:
-            crrIndex += 1
+            crr_index += 1
             prev = nxt
             nxt = node
 
-            if crrIndex == index:
+            if crr_index == index:
                 new_link = LinkedList(value, nxt)
                 prev.next = new_link
 
                 return self
 
     def erase(self, index):
-        crrIndex = -1
+        crr_index = -1
         prev, nxt = None, self
 
         for node in self:
-            crrIndex += 1
+            crr_index += 1
             prev = nxt
             nxt = node
 
-            if crrIndex == index:
+            if crr_index == index:
                 prev.next = nxt.next
 
                 return self
+
+    def reverse(self):
+        prev = None
+        node = copy.deepcopy(self)
+
+        while node is not None:
+            nxt = node.next
+            node.next = prev
+            prev = node
+            node = nxt
+
+        return prev
+
+    def value_n_from_end(self, n):
+        return self.reverse().value_at(n)
+
+    def remove_value(self, value):
+        prev = None
+        node = self
+
+        while node is not None:
+            if node.value == value:
+                prev.next = node.next
+
+                return node.value
+
+            prev = node
+            node = node.next
 
 
 node = LinkedList(5, LinkedList(6, LinkedList(7, LinkedList(8))))
@@ -128,9 +159,14 @@ print(node.value_at(2))
 print(node.push_front(4))
 print(node.pop_front())
 print(node.push_back(9))
+print(node.push_back(10))
+print(node.push_back(11))
 print(node.pop_back())
 print(node.front())
 print(node.back())
 print(node.insert(2, 6.5))
 print(node.erase(2))
+print(node.value_n_from_end(2))
+print(node.reverse())
+print(node.remove_value(7))
 print(node)
